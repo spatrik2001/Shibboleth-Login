c6ca1f173d081e9375251439afb034dd4dd38265
spatrik2001 <scheuer.patrik@gmail.com>
2024-06-26 15:02:40 +0200

    Kicsi finomítás

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -6,13 +6,15 @@ export default function Login() {
         <main className={styles.shibMain}>
             <div className={styles.shib_panel}>
                 <div className={styles.logo}>
-                    <Image src="/sze_logo.png" alt="Széchenyi István Egyetem logo" width="170" height="60"/>
+                    <a href="/">
+                        <Image src="/sze_logo.png" alt="Széchenyi István Egyetem logo" width="170" height="60"/>
+                    </a>
                 </div>
                 <div className={styles.login_box}>
                     <h1 className={styles.header}>Kérjük jelentkezzen be!</h1>
                     <hr/>
                     <h3>Please login!</h3>
-                    <form action="http://idp.localhost/idp/shibboleth" method="post">
+                    <form action="/login" method="post">
                     <input className={styles.userinput} type="text" name="username" id="username" placeholder="Felhasználónév / Username" required/>
                     <input className={styles.userinput} type="password" name="password" id="password" placeholder="Jelszó / Password" required/>
                     <input className={styles.userbutton} type="submit" value="Belépés / Login"/>
@@ -20,8 +22,13 @@ export default function Login() {
                     <input type="hidden" name="RelayState" value="<url>"/>
                     </form>
                     <div className={styles.bottom_informations}>
-                    <Image src="/logo_eduid.png" alt="eduID logo" width={64} height={25}/>
-                    <Image src="/logo_shi.png" alt="Shibboleth logo" width={72} height={25}/>
+                        <div className={styles.faq}>
+                            <a href="#">Adatvédelem</a>
+                        </div>
+                        <div className={styles.icons}>
+                            <Image src="/logo_eduid.png" alt="eduID logo" width={64} height={25}/>
+                            <Image src="/logo_shi.png" alt="Shibboleth logo" width={72} height={25}/>
+                        </div>
                     </div>
                 </div>
             </div>
